0fd41bb220947de8d18e37700b8b3040baf20f58
Barry Bridgens <barry@bridgens.me.uk>
2019-04-20 11:45:19 +0100

    Updated with ideas from Mike Zamansky on YouTube

--- init.el
+++ init.el
@@ -8,17 +8,88 @@
 ;;; emacs setup
 
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
+(setq inhibit-startup-message t)
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (package-initialize)
 
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+(package-refresh-contents)
+(package-install 'use-package))
+
+(use-package try
+  :ensure t)
+
+(use-package which-key
+  :ensure t
+  :config
+  (which-key-mode))
+
+(use-package counsel
+  :ensure t
+)
+
+(use-package swiper
+  :ensure try
+  :config
+  (progn
+	(ivy-mode 1)
+	(setq ivy-use-virtual-buffers t)
+	(global-set-key "\C-s" 'swiper)
+	(global-set-key (kbd "C-c C-r") 'ivy-resume)
+	(global-set-key (kbd "<f6>") 'ivy-resume)
+	(global-set-key (kbd "M-x") 'counsel-M-x)
+	(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+	(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+	(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+	(global-set-key (kbd "<f1> l") 'counsel-load-library)
+	(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+	(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+	(global-set-key (kbd "C-c g") 'counsel-git)
+	(global-set-key (kbd "C-c j") 'counsel-git-grep)
+	(global-set-key (kbd "C-c k") 'counsel-ag)
+	(global-set-key (kbd "C-x l") 'counsel-locate)
+	(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+	(define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
+	))
+
+(use-package avy
+  :ensure t
+  :bind ("M-s" . avy-goto-char))
+
+(use-package auto-complete
+  :ensure t
+  :init
+  (progn
+	(ac-config-default)
+	(global-auto-complete-mode t)
+	))
+
 (set-scroll-bar-mode 'right)
-(ido-mode t)
 
-;; Color Theme
+;(setq ido-enable-flex-matching t)
+;(setq ido-everywhere t)
+;(ido-mode t)
 
+(defalias 'list-buffers 'ibuffer)
+
+;; Theme
+(use-package zenburn-theme
+  :ensure t
+  :config (load-theme 'zenburn t))
+
+;;; Evil mode
+;; goto-chg
+;(add-to-list 'load-path "~/.emacs.d/goto-chg")
+;(require 'goto-chg)
+
+;; Evil mode
+;(add-to-list 'load-path "~/.emacs.d/evil")
+;(require 'evil)
+;(evil-mode 1)
 
 
 ;; Org mode
@@ -29,8 +100,18 @@
 (global-set-key "\C-cb" 'org-iswitchb)
 
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ ;'(custom-enabled-themes (quote (tango-dark)))
+ '(org-agenda-files (list org-directory))
  '(org-directory "~/Dropbox/Documents/org")
- '(org-agenda-files (list org-directory)))
+ '(package-selected-packages
+   (quote
+	(zenburn-theme auto-complete avy try use-package slime)))
+
+ '(tool-bar-mode nil))
 
 (setq org-agenda-dim-blocked-tasks 'invisible)
 (setq org-agenda-todo-ignore-deadlines (quote far))
@@ -53,13 +134,7 @@
 
 
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (tango-dark)))
- '(tool-bar-mode nil))
+
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
